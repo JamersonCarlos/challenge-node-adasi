4d76c052f5ff73cc9cd4ade1c849c30c1db59497
Jamerson Carlos <64469941+JamersonCarlos@users.noreply.github.com>
2024-10-02 00:13:11 -0300

    endpoint para criar curso

--- api/cursos.js
+++ api/cursos.js
@@ -2,16 +2,23 @@ const express = require('express');
 const router = express.Router();
 const Curso = require('../models/curso');
 
+// Rota para criar um curso 
 router.post('', async (req, res) => { 
     const { nome } = req.body; 
-    try {
-        const novoCurso = await Curso.create({ nome });
-        return res.status(201).json(novoCurso);
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ message: 'Erro ao inserir curso', error });
-    }
+    Curso.findAll({where: { nome }}).then(async cursos => { 
+        if(cursos.length == 0) { 
+            try {
+                const novoCurso = await Curso.create({ nome });
+                return res.status(201).json(novoCurso);
+            } catch (error) {
+                return res.status(500).json({ message: 'Erro ao inserir curso', error });
+            }
+        } else { 
+            return res.status(409).json({ message: "Esse curso já existe!"});
+        }
+    }).catch(error => res.status(500).json({message: "Erro ao buscar cursos"}, error));
 });
+
 
 module.exports = router; 
 
