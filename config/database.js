e18aef54edb74a513b31dfad7b124a5fc7ee9b1e
Jamerson Carlos <64469941+JamersonCarlos@users.noreply.github.com>
2024-10-02 02:12:07 -0300

    trocando por variáveis dinamicas, de acordo com o arquivo .env

--- config/database.js
+++ config/database.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 module.exports = { 
-    dialect: 'postgres',  
-    host: 'localhost',
-    port: 5432, 
-    database: 'node-challenge',
-    username: 'postgres',
-    password: 'carlos'
+    dialect: process.env.DB_DIALECT,  
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT, 
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PWD
 }
