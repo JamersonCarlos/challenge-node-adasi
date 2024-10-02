a6f58d490e364e412c5677e9e6550e40ad56c91f
Jamerson Carlos <64469941+JamersonCarlos@users.noreply.github.com>
2024-10-02 00:20:07 -0300

    endpoint para criar tarefa

--- api/tarefas.js
+++ api/tarefas.js
@@ -1,5 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
+router.post('', async (req, res) => { 
+    const { nome } = req.body; 
+    Tarefa.findAll({where: { nome }}).then(async tarefas => { 
+        if(tarefas.length == 0) { 
+            try {
+                const novaTarefa = await Tarefa.create({ nome });
+                return res.status(201).json(novaTarefa);
+            } catch (error) {
+                return res.status(500).json({ message: 'Erro ao inserir tarefa', error });
+            }
+        } else { 
+            return res.status(409).json({ message: "Essa tarefa já existe!"});
+        }
+    }).catch(error => res.status(500).json({message: "Erro ao buscar tarefas"}, error));
+});
+
+
 module.exports = router; 
 
